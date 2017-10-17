55bfea556a48b105bc9cdcc537e89443de7f1a77
admin <admin@gmail.com>
2017-10-17 16:44:54 -0500

    lte

--- album/admin.py
+++ album/admin.py
@@ -4,6 +4,10 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from album.models import Category, Photo
 
+import sys 
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Photo)
--- myapps/settings.py
+++ myapps/settings.py
@@ -31,6 +31,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+     #para poder ejecutar adminlte y hacer uso de sus templates
+    'django_adminlte',
+    #si se requiere cambiar la interfaz del admin propio de django
+    'django_adminlte_theme',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
