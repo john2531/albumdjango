b05bd958ab379c82a379e163162c946e828cb254
admin <admin@gmail.com>
2017-11-07 16:38:30 -0500

    login requiremnt

--- album/views.py
+++ album/views.py
@@ -5,6 +5,7 @@ from album.models import Category, Photo
 from django.views.generic import ListView, DetailView
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic.edit import UpdateView, CreateView, DeleteView 
+from django.contrib.auth.decorators import login_required
 
 from django.shortcuts import render
 
@@ -12,35 +13,44 @@ from django.shortcuts import render
 def first_view(request):
     return HttpResponse('Esta es mi primera vista!')
 
+@login_required
 def category(request):
     category_list = Category.objects.all()
     context = {'object_list': category_list}
     return render(request, 'album/category.html', context)
 
+@login_required
 def category_detail(request, category_id):
     category = Category.objects.get(id=category_id)
     context = {'object': category}
     return render(request, 'album/category_detail.html', context)
 
-class PhotoListView(ListView):
+class LoginRequiredMixin(object):
+    @classmethod
+    def as_view(cls):
+        return login_required(super(LoginRequiredMixin, cls).as_view())
+
+class PhotoListView(LoginRequiredMixin, ListView):
     model = Photo
 
-class PhotoDetailView(DetailView):
+class PhotoDetailView(LoginRequiredMixin,DetailView):
     model = Photo
+
 
 def base(request):
     return render(request, 'base.html')
 
-class PhotoUpdate(UpdateView):
+
+class PhotoUpdate(LoginRequiredMixin,UpdateView):
     model = Photo
     fields='__all__'
 
-class PhotoCreate(CreateView):
+class PhotoCreate(LoginRequiredMixin,CreateView):
     model = Photo
     fields='__all__'
     
 
-class PhotoDelete(DeleteView):
+class PhotoDelete(LoginRequiredMixin,DeleteView):
     model = Photo
     success_url = reverse_lazy('photo-list')
     
--- myapps/settings.py
+++ myapps/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from django.core.urlresolvers import reverse_lazy
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -47,6 +48,8 @@ INSTALLED_APPS = [
     'album',
 ]
 
+LOGIN_REDIRECT_URL = "/album"
+LOGOUT_URL = "/accounts/logout"
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
 MIDDLEWARE = [
